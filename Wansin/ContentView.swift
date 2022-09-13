dc851ffc5f556acd18357e82d0af6d39ca8df1f9
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-09-13 21:08:11 +0200

    Initial Commit

---
+++ Wansin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wansin
+//
+//  Created by Aitor Zubizarreta on 13/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wansin/WansinApp.swift
@@ -0,0 +1,17 @@
+//
+//  WansinApp.swift
+//  Wansin
+//
+//  Created by Aitor Zubizarreta on 13/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WansinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
